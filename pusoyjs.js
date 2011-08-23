2b8bf694c67d2899542716aff94cc01c3fce53ca
Nelson Monterroso <nelsonmonterroso@gmail.com>
2011-08-23 13:36:06 -0700

    use redis for session store

--- pusoyjs.js
+++ pusoyjs.js
@@ -4,14 +4,18 @@
  */
 require('ejs');
 var express = require('express'),
-  app = module.exports = express.createServer(),
-  io = require("socket.io").listen(app),
-  MemcachedStore = require('connect-memcached'),
-  session_store;
+    connect = require('connect'),
+    app = module.exports = express.createServer(),
+    io = require("socket.io").listen(app),
+    RedisStore = require('connect-redis')(express),
+    session_store = new RedisStore({
+      host: '127.0.0.1',
+      port: 6379
+    });
+
 
 // Configuration
-
-app.configure(function(){
+app.configure(function() {
   app.set('listen_port', 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -22,32 +26,22 @@ app.configure(function(){
   });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: "Pu$0Yi$4w3s0m3", 
+    store: session_store,
+    key: 'express.sid'
+  }));
+  app.use(app.router);
 });
 
 app.configure('development', function(){
-  session_store = new express.session.MemoryStore();
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-  app.use(express.session({
-    secret: "Pu$0Yi$4w3s0m3", 
-    key: 'express.sid',
-    store: session_store,
-  }));
 });
 
 app.configure('production', function(){
-  session_store = new MemcachedStore({
-    hosts: [
-      '127.0.0.1:11211'
-    ]
-  });
-  app.use(express.session({
-    secret: "Pu$0Yi$4w3s0m3", 
-    key: 'express.sid',
-    store: session_store,
-  }));
   app.use(express.errorHandler()); 
 });
 
